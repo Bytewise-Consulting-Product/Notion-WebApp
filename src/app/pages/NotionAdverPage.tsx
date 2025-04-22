f081b267f07764bfb6959f15cb42d46387c6b825
kishu279 <souravpoddar6677@gmail.com>
2025-04-22 19:54:04 +0530

    ready to build

--- src/app/pages/NotionAdverPage.tsx
+++ src/app/pages/NotionAdverPage.tsx
@@ -1,9 +1,9 @@
 export default async function NotionAdverPage() {
-  // console.log("User : ", user);
   return (
     <>
       <div>
         <div className="h-[100px] text-5xl">NOTION</div>
+        <div>Add private pages </div>
       </div>
     </>
   );
